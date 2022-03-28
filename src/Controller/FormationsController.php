3a32626b8c63a3993071fc7f875bd728c14ef526
cathyLG <catherine.x.sart@gmail.com>
2022-03-28 23:54:01 +0200

    phase 3 : ajouter une liste déroulante de niveaux dans formations.html.twig

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -6,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\FormationRepository;
+use App\Repository\NiveauRepository;
 
 /**
  * Description of FormationsController
@@ -13,7 +15,7 @@ use App\Repository\FormationRepository;
  * @author emds
  */
 class FormationsController extends AbstractController {
-    
+
     private const PAGEFORMATIONS = "pages/formations.html.twig";
 
     /**
@@ -23,63 +25,73 @@ class FormationsController extends AbstractController {
     private $repository;
 
     /**
+     *
+     * @var NiveauRepository
+     */
+    private $repositoryN;
+
+    /**
      * 
      * @param FormationRepository $repository
      */
-    function __construct(FormationRepository $repository) {
+    function __construct(FormationRepository $repository, NiveauRepository $repositoryN) {
         $this->repository = $repository;
+        $this->repositoryN = $repositoryN;
     }
 
     /**
      * @Route("/formations", name="formations")
      * @return Response
      */
-    public function index(): Response{
+    public function index(): Response {
         $formations = $this->repository->findAll();
+        $niveaux = $this->repositoryN->findAll();
         return $this->render(self::PAGEFORMATIONS, [
-            'formations' => $formations
+                    'formations' => $formations,
+                    'niveaux' => $niveaux
         ]);
     }
-    
+
     /**
      * @Route("/formations/tri/{champ}/{ordre}", name="formations.sort")
      * @param type $champ
      * @param type $ordre
      * @return Response
      */
-    public function sort($champ, $ordre): Response{
+    public function sort($champ, $ordre): Response {
         $formations = $this->repository->findAllOrderBy($champ, $ordre);
         return $this->render(self::PAGEFORMATIONS, [
-           'formations' => $formations
+                    'formations' => $formations
         ]);
-    }   
-        
+    }
+
     /**
      * @Route("/formations/recherche/{champ}", name="formations.findallcontain")
      * @param type $champ
      * @param Request $request
      * @return Response
      */
-    public function findAllContain($champ, Request $request): Response{
-        if($this->isCsrfTokenValid('filtre_'.$champ, $request->get('_token'))){
+    public function findAllContain($champ, Request $request): Response {
+        if ($this->isCsrfTokenValid('filtre_' . $champ, $request->get('_token'))) {
             $valeur = $request->get("recherche");
             $formations = $this->repository->findByContainValue($champ, $valeur);
             return $this->render(self::PAGEFORMATIONS, [
-                'formations' => $formations
+                        'formations' => $formations
             ]);
         }
         return $this->redirectToRoute("formations");
-    }  
-    
+    }
+
     /**
      * @Route("/formations/formation/{id}", name="formations.showone")
      * @param type $id
      * @return Response
      */
-    public function showOne($id): Response{
+    public function showOne($id): Response {
         $formation = $this->repository->find($id);
         return $this->render("pages/formation.html.twig", [
-            'formation' => $formation
-        ]);        
-    }    
+                    'formation' => $formation
+        ]);
+    }
+
 }
