932163607f6e6b05393cb2625d55291408410d16
cathyLG <catherine.x.sart@gmail.com>
2022-03-29 11:19:26 +0200

    phase 5 : créer admin.formations.html.twig avec la même présentation de formations front, et créer AdminFormationsController

---
+++ src/Controller/admin/AdminFormationsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Niveau;
+use App\Repository\NiveauRepository;
+
+/**
+ * Description of AdminFormationsController
+ *
+ * @author Xiaoxiao LIU
+ */
+class AdminFormationsController extends AbstractController {
+
+    /**
+     *
+     * @var FormationRepository
+     */
+    private $repository;
+
+    /**
+     * @var Niveau[]
+     */
+    private $niveaux;
+
+    /**
+     * 
+     * @param FormationRepository $repository
+     */
+    public function __construct(FormationRepository $repository, NiveauRepository $repositoryN) {
+        $this->repository = $repository;
+        $this->niveaux=$repositoryN->findAll();
+    }
+
+    /**
+     * @Route("/admin", name="admin.formations")
+     * @return Response
+     */
+    public function index(): Response {
+        $formations = $this->repository->findAll();
+        return $this->render("admin/admin.formations.html.twig", [
+                    'formations' => $formations,
+                    'niveaux' => $this->niveaux
+        ]);
+    }
+
+}
